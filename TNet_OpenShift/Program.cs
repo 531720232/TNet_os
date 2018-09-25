be644437444a10498d8a209c0d68c609929a3524
fyindex <35559908+531720232@users.noreply.github.com>
2018-09-25 14:05:16 +0800

    a1

---
+++ TNet_OpenShift/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TNet_OpenShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new TNet.Runtime.TNServer_AppInConsole();
+            app.StartApplication();
+
+
+
+
+            while (true)
+            {
+
+
+
+            }
+
+        }
+    }
+}
